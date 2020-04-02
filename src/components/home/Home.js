ed5f3ac2c3d2220018f73202fbf759e1dd08ed4b
Natesuri <natesuri@gmail.com>
2020-04-02 14:32:46 -0400

    Refactors Home to stateful component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { COPY } from '../../constants/constants'
 import { formatPageCopy } from '../../utils'
+export class HomePage extends React.Component {
 
-export const HomePage = () => {
-  return(
-    <React.Fragment>
-      { formatPageCopy(COPY.HOME) }
-    </React.Fragment>
-  )
+  state = {
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        { formatPageCopy(COPY.HOME) }
+      </React.Fragment>
+    )
+  }
+
 }
