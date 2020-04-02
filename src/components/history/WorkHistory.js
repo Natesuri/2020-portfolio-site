31f3701a26f5a9188b0b2db9c87bf85c894cb8dd
Natesuri <natesuri@gmail.com>
2020-04-02 19:55:13 -0400

    Tenure Time

--- src/components/history/WorkHistory.js
+++ src/components/history/WorkHistory.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { COPY } from '../../constants/constants'
 import { formatPageCopy } from '../../utils'
+import { TenureTimer } from './TenureTimer'
+
+const generateTenureTimers = events => {
+  return events.map((lifeEvent, index) => {
+    return <TenureTimer key={lifeEvent + index} pointInTime={lifeEvent.eventDate} eventName={lifeEvent.eventTitle}/>
+  })
+}
 
 export const WorkHistory = () => {
   return(
     <React.Fragment>
       { formatPageCopy(COPY.HISTORY) }
+      <div className='workBlockContainer'>
+        { generateTenureTimers(COPY.HISTORY.EVENTS) }
+      </div>
     </React.Fragment>
   )
 }
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -57,6 +57,20 @@ export const COPY = {
       "2019: I joined Wayfair just 6 weeks after the birth of my first child. I quickly learned the stack and joined the Asia Logistics team within the International Supply Chain department. While there, I built and improved on several different internal full-stack applications written in  React and PHP. These applications focused on increasing the efficiency of our operations team, and automating time-consuming logistics tasks.",
       "October 2018: Having dabbled in programming on-and-off throughout the previous couple years, I finally decided to pursue programming as a career. I started the General Assembly Software Engineering Immersive bootcamp in October 2018 and over the course of three months I wrote 3 full-stack applications while learning several languages and frameworks, and learning how to problem solve like a programmer.",
       "Pre-Bootcamp: Between May 2015 and October 2018 I worked as an editor and assistant editor in documentary film, corporate video, and network TV. As an assistant editor I was responsible for media management, project organization, quality control, and discovering new techniques and technologies to make the editor's life easier. As an editor, it was my job to find the story in the material I was given. Whether crafting a feature film or 30 second highlight, I needed to create a visually and emotionally compelling story."
+    ],
+    EVENTS: [
+      {
+        eventDate: "2019-06-03 08:00:00",
+        eventTitle: "Tenure at Wayfair"
+      },
+      {
+        eventDate: "2018-10-03 09:00:33",
+        eventTitle: "Time since I began my engineering career"
+      },
+      {
+        eventDate: "2017-04-23 17:30:47",
+        eventTitle: "Time since Get Me Roger Stone was released"
+      },
     ]
   },
   PERSONAL: {
