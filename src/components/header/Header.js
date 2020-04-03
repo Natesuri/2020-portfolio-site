a05f2613206ff58b388883eaeccc2b8cf321fc8a
Natesuri <natesuri@gmail.com>
2020-04-02 20:43:13 -0400

    Create NavItem to allow dynamic styling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,30 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { NAV } from '../../constants/constants'
+import { NavItem } from './NavItem'
 
 import './Header.scss'
 
-export const Header = () => (
-    <header className='main-header' >
-      <nav>
-        <Link to={ NAV.HOME.route }>
-          { NAV.HOME.name }
-        </Link>
-        <Link to={ NAV.HISTORY.route }>
-          { NAV.HISTORY.name }
-        </Link>
-        { /* <Link to={ NAV.SKILLS.route }>
-          { NAV.SKILLS.name }
-        </Link> */ }
-        <Link to={ NAV.PERSONAL.route }>
-          { NAV.PERSONAL.name }
-        </Link>
-        <Link to={ NAV.CONTACT.route }>
-          { NAV.CONTACT.name }
-        </Link>
-        <Link to={ NAV.RESUME.route }>
-          { NAV.RESUME.name }
-        </Link>
-      </nav>
-    </header>
-)
+export const Header = (props) => {
+    return (
+      <header className='main-header' >
+        <nav>
+          <NavItem activePath={props.history.location.pathname} page={NAV.HOME}/>
+          <NavItem activePath={props.history.location.pathname} page={NAV.HISTORY}/>
+          <NavItem activePath={props.history.location.pathname} page={NAV.PERSONAL}/>
+          <NavItem activePath={props.history.location.pathname} page={NAV.CONTACT}/>
+          <NavItem activePath={props.history.location.pathname} page={NAV.RESUME}/>
+        </nav>
+      </header>
+    )
+  }
